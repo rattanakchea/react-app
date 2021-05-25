a6de10e09d63631e70016829eab73f78b3342c11
Rattanak Chea <rattanak.chea@dpworld.com>
2021-05-24 21:19:10 -0700

    addNumber

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -4,10 +4,16 @@ import React, { useState } from "react";
 function Counter() {
     const [number, setNumber] = useState(0);
 
+    const addNumber = () => {
+        setNumber(number + 1);
+    };
+
     // jsx
     return (
         <div>
             <h3>Counter: {number}</h3>
+
+            <button onClick={addNumber}>Add a number</button>
         </div>
     )
 }
